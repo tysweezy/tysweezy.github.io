4a5351cc2837c88968eb70369f00ab1213689cc4
tysweezy <tsouza.sf@gmail.com>
2020-12-10 17:55:24 -0800

    fdasfdasf

--- _site/assets/js/search-data.js
+++ _site/assets/js/search-data.js
@@ -2,45 +2,45 @@ var store = [{
         "title": "Quick Thoughts on Test-Driven Development and Design Patterns",
         "excerpt":"As a developer, a lot of times I feel constantly worried that I’m doing the “wrong thing” when I’m writing code. I worry if I’m doing “x” correctly, while I’m trying to focus on “y” thing. This mostly occurs for me when terms such as “test-driven development” and “design patterns”...","categories": ["tdd","rant"],
         "tags": [],
-        "url": "http://localhost:4000/tdd/rant/2015/12/10/thoughts-on-tdd.html"
+        "url": "http://tylersouza.com/tdd/rant/2015/12/10/thoughts-on-tdd.html"
       },{
         "title": "Pondering About My Main Backend Techstack of Choice",
         "excerpt":"For about eight months or so, I decided to commit myself and focus on Node.js as my main backend techstack of choice. PHP/Laravel have treated me well and I’ve been loving Go, but I wanted to focus on one tech stack, which is now Node as my main go-to. There’s...","categories": ["javascript","techstack"],
         "tags": [],
-        "url": "http://localhost:4000/javascript/techstack/2018/07/07/pondering-techstack.html"
+        "url": "http://tylersouza.com/javascript/techstack/2018/07/07/pondering-techstack.html"
       },{
         "title": "Part 1: Wavvy, My Open Source Project Idea. Providing REST API Helpers and Syntactic Sugar",
         "excerpt":"I’ve decided to start an open source project and fill another repository on my GitHub. Since I’ve been working more on the backend side and building services/REST APIs, I’ve had an urge to dedicate myself in this area. I thought it would a wonderful area to provide tooling for REST...","categories": ["python","opensource","api"],
         "tags": [],
-        "url": "http://localhost:4000/python/opensource/api/2018/08/08/wavvy-api-tools.html"
+        "url": "http://tylersouza.com/python/opensource/api/2018/08/08/wavvy-api-tools.html"
       },{
         "title": "Some Philosophies on Separating Your Stack",
         "excerpt":"For web applications, it’s a common pattern to separate your backend (typically a REST API) from your front-end/user interface nowadays. It’s a practice that I’ve been using for all of my projects for the past year or so. I think this is a good idea for a number of reasons:...","categories": ["ramblings","software","engineering"],
         "tags": [],
-        "url": "http://localhost:4000/ramblings/software/engineering/2018/11/06/some-thoughts-on-separating-tech-stack.html"
+        "url": "http://tylersouza.com/ramblings/software/engineering/2018/11/06/some-thoughts-on-separating-tech-stack.html"
       },{
         "title": "My “A-ha” Moment with Go - Understanding the Interface",
         "excerpt":"I’ve been using and toying with Go for almost two years now. At the time I decided to learn the language, I was curious on what this language had to offer. Initially, I was really intrigued by it’s simplicity. Loved that it was minimal and easy to pick up. I...","categories": ["software","engineering","go"],
         "tags": [],
-        "url": "http://localhost:4000/software/engineering/go/2019/01/29/my-aha-moment-with-go.html"
+        "url": "http://tylersouza.com/software/engineering/go/2019/01/29/my-aha-moment-with-go.html"
       },{
         "title": "Slack Bot Idea: Working with Some Data with Python",
         "excerpt":"A lot of times at the office around lunch time, one of us will ask everyone what their lunch plans are via Slack. This leads to one or a couple of us to take in orders and go pick up lunch a group of co-workers. I’ve been interested in building...","categories": ["software","engineering","python"],
         "tags": [],
-        "url": "http://localhost:4000/software/engineering/python/2019/06/07/slack-bot-lunch-intial-data.html"
+        "url": "http://tylersouza.com/software/engineering/python/2019/06/07/slack-bot-lunch-intial-data.html"
       },{
         "title": "New Year Goals. A Reflection.",
         "excerpt":"2019 really had it’s ups-and-downs. It sure tested me as a living being. It was a wild year and it made me think what is REALLY important in my life; everything ranging from my own development career to friends and family. That being said, I’m glad the year is behind...","categories": ["refelction","Goals"],
         "tags": [],
-        "url": "http://localhost:4000/refelction/goals/2020/01/01/new-year-goals-a-reflection.html"
+        "url": "http://tylersouza.com/refelction/goals/2020/01/01/new-year-goals-a-reflection.html"
       },{
         "title": "So I Want to Learn How to Build a Compiler",
         "excerpt":"I love learning and talking about programming languages. Mostly enjoy a different way of thinking that comes with it. Bring up Haskell? We’ll talk about Monads and functional programming. Clojure? Let’s talk about transducers. Bringing up Go? Let’s talk about go routines, concurrency and striving for simplicity. Why Would I...","categories": ["software","engineering","languages"],
         "tags": [],
-        "url": "http://localhost:4000/software/engineering/languages/2020/05/10/so-i-want-to-learn-how-to-build-a-compiler.html"
+        "url": "http://tylersouza.com/software/engineering/languages/2020/05/10/so-i-want-to-learn-how-to-build-a-compiler.html"
       },{
         "title": "Processing Large Tasks with Laravel Queues",
         "excerpt":"Recently, I tackled an interesting problem with an app I’m building. Just for context and without going into too much detail, this system is a management admin for guidance counselors to help manage their patients who are students in High School. I had to build a feature that generated five...","categories": ["software","engineering"],
         "tags": [],
-        "url": "http://localhost:4000/software/engineering/2020/07/12/processing-large-tasks-with-laravel-queues.html"
+        "url": "http://tylersouza.com/software/engineering/2020/07/12/processing-large-tasks-with-laravel-queues.html"
       }]
